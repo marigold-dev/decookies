c566cfb76f489e96896b95d722914dae1523621b
oteku <thomas.haessle@gmail.com>
2022-07-13 18:24:54 +0200

    :tada: simple boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import cookie from './perfectCookie.png';
 import './App.css';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const creacteTransaction = (node_uri, public_key, private_key, payload ) => {
+
+// }
+
+interface Values {
+  publicKey: string,
+  privateKey: string,
+  nodeUri: string
 }
+
+
+const App = () => (
+  <div className="App">
+    <Formik
+      initialValues={{
+        publicKey: '',
+        privateKey: '',
+        nodeUri: 'http://localhost:4440/',
+      }}
+      onSubmit={(
+        values: Values,
+        { setSubmitting }: FormikHelpers<Values>
+      ) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+
+      <Form>
+        <label htmlFor="publicKey">Public Key</label>
+        <Field id="publicKey" name="publicKey" />
+
+        <label htmlFor="privateKey">Private Key</label>
+        <Field id="privateKey" name="privateKey" />
+
+        <label htmlFor="privateKey">Private Key</label>
+        <Field id="privateKey" name="privateKey" />
+        <button type="submit">
+          <img src={cookie} className="App-logo" alt="logo" /></button>
+      </Form>
+    </Formik>
+  </div>
+);
+
 
 export default App;
