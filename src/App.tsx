12f4af7c2b38485b6be61852d917cf0665d69f01
oteku <thomas.haessle@gmail.com>
2022-07-15 15:10:23 +0200

    :construction_worker:

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,27 @@ import cookie from './perfectCookie.png';
 import './App.css';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 import { InMemorySigner } from '@taquito/signer';
-import { randomInt } from 'crypto';
-import blake2b from 'blake2b';
-// const creacteTransaction = (node_uri, public_key, private_key, payload ) => {
-
-// }
-
+import { encodeExpr, buf2hex } from '@taquito/utils';
+// import blake2b from 'blake2b';
 interface Values {
   address: string,
-  publicKey: string,
   privateKey: string,
   nodeUri: string
 }
 
-const signBytes = (privateKey: string, sbytes: string) => {
-  const signer = new InMemorySigner(privateKey);
+const signBytes = (signer: InMemorySigner, privateKey: string, sbytes: string) => {
   signer.sign(sbytes);
 }
 
-const hashBlake2b = (payload: string): string => {
-  const output = new Uint8Array(64);
+// const hashBlake2b = (payload: string): string => {
+//   const output = new Uint8Array(64);
+//   const input = Buffer.from(payload);
+//   return blake2b(output.length).update(input).digest('hex');
+// }
+
+const stringToHex = (payload: string): string => {
   const input = Buffer.from(payload);
-  return blake2b(output.length).update(input).digest('hex');
+  return buf2hex(input);
 }
 
 const handleSubmit = async (
@@ -32,31 +31,42 @@ const handleSubmit = async (
   { setSubmitting }: FormikHelpers<Values>
 ) => {
 
-  const OPERATION = "user-operation-gossip"
-  const key = values.publicKey;
-  const signature = signBytes(values.privateKey, "mabite");
-  const nonce = randomInt(1000000);
+  const signer = new InMemorySigner(values.privateKey);
+  // const OPERATION = "user-operation-gossip"
+
   try {
+    const key = await signer.publicKey();
+    const nonce = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
     // const block_r = await fetch(values.nodeUri + "/block-level")
     // const block_j = await block_r.json()
     // const block_height: number = block_j.level;
-    const block_height = 0;
+    const block_height = 0; // change to get the real height
     const payload = "cookie";
     const data = {
       source: values.address,
       initial_operation: ["Vm_transaction", {
         payload
       }],
-      hash: hashBlake2b(payload),
+      hash: encodeExpr(stringToHex(payload)).substring(4), //remove expr prefix
     }
+    const hash_payload = [
+      nonce.toString(),
+      block_height.toString(),
+      JSON.stringify(data)
+    ];
+    const hash = encodeExpr(stringToHex(JSON.stringify(hash_payload))).substring(4); //remove expr prefix
+    const signature = signBytes(signer, values.privateKey, hash);
+
     const operation = {
-      hash: hashBlake2b(JSON.stringify(data)),
+      hash,
       key,
       signature,
       nonce,
-      block_height
+      block_height,
+      data
     }
 
+    // ADD POST ACTION BELOW
     setTimeout(() => {
       alert(JSON.stringify(operation, null, 2));
       setSubmitting(false);
@@ -72,7 +82,6 @@ const App = () => (
     <Formik
       initialValues={{
         address: '',
-        publicKey: '',
         privateKey: '',
         nodeUri: 'http://localhost:4440/',
       }}
@@ -83,8 +92,6 @@ const App = () => (
         <label htmlFor="address">Address</label>
         <Field id="address" name="address" />
 
-        <label htmlFor="publicKey">Public Key</label>
-        <Field id="publicKey" name="publicKey" />
 
         <label htmlFor="privateKey">Private Key</label>
         <Field id="privateKey" name="privateKey" />
