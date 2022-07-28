a422027c0d8c0ccaf6065b1a30bbb6ab94b3fbc6
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-07-28 15:15:59 +0200

    farms + not working beacon-wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import cookie from '../resources/perfectCookie.png';
 import cursor from '../resources/cursor.png';
 import grandma from '../resources/grandma.png';
+import farm from '../resources/farm.png';
 import './App.css';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 import { InMemorySigner } from '@taquito/signer';
-import { encodeExpr, buf2hex, b58decode } from '@taquito/utils';
+import { encodeExpr, buf2hex, b58decode, char2Bytes } from '@taquito/utils';
+import { TezosToolkit } from "@taquito/taquito";
+// import { BeaconWallet } from "@taquito/beacon-wallet";
+// import { TezosOperationType } from "@airgap/beacon-sdk";
+
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import { NetworkType } from '@airgap/beacon-types';
+import { type } from 'os';
+import { add } from 'libsodium-wrappers';
+
+const options = { name: 'exampleWallet' };
+const wallet = new BeaconWallet(options);
+
+
+const Tezos = new TezosToolkit("https://mainnet-tezos.giganode.io");
+// const wallet = new BeaconWallet({ name: "Beacon Docs Taquito" });
+
+// Tezos.setWalletProvider(wallet);
+
+let myAddress: string | undefined;
 
 interface Values {
   address: string,
@@ -13,11 +33,14 @@ interface Values {
   cookies: number
   cursors: number
   grandmas: number
+  farms: number
   cursor_cost: number
   grandma_cost: number
+  farm_cost: number
   cps: number
   cursor_cps: number
   grandma_cps: number
+  farm_cps: number
 }
 
 enum action_type {
@@ -60,7 +83,7 @@ const get_actual_state = async (values: Values) => {
   const state_response = await state_request.json();
   console.log(JSON.stringify(state_response));
   const value = state_response.state.filter(([address, game_state]) => address === values.address);
-  if (value.length != 1) {
+  if (value.length !== 1) {
     console.error("More than one record for this address: " + values.address);
     alert("More than one record for this address: " + values.address);
   } else {
@@ -71,6 +94,29 @@ const get_actual_state = async (values: Values) => {
 
 
 const mint = async (values: Values, actions: FormikHelpers<Values>, action: action_type) => {
+
+  wallet
+    .requestPermissions({ network: { type: NetworkType.JAKARTANET } })
+    .then((_) => wallet.getPKH())
+    .then((address) => {
+      console.log(`Your address: ${address}`);
+      values.address = address;
+    });
+
+  Tezos.setWalletProvider(wallet);
+
+  // const activeAccount = await wallet.client.getActiveAccount();
+  // if (activeAccount) {
+  //   // If defined, the user is connected to a wallet.
+  //   // You can now do an operation request, sign request, or send another permission request to switch wallet
+  //   console.log("Already connected:", activeAccount.address);
+  //   myAddress = activeAccount.address;
+  // } else {
+  //   await wallet.requestPermissions();
+  //   myAddress = await wallet.getPKH();
+  //   console.log("New connection:", myAddress);
+  // }
+
 
   console.log("Mint cookie");
   const signer = new InMemorySigner(values.privateKey);
@@ -99,18 +145,38 @@ const mint = async (values: Values, actions: FormikHelpers<Values>, action: acti
     ]);
 
     const outer_hash = b58decode(encodeExpr(stringToHex(full_payload))).slice(4, -2);
-    const signature = await signer.sign(stringToHex(full_payload)).then((val) => val.prefixSig);
+    const signature_taquito = await signer.sign(stringToHex(full_payload)).then((val) => val.prefixSig);
     const operation = {
       hash: outer_hash,
       key,
-      signature,
+      signature_taquito,
       nonce,
       block_height,
       data
     }
     const packet =
       { user_operation: operation };
-    const result = await fetch(values.nodeUri + user_operation_gossip,
+
+    // const formattedInput: string = [
+    //   'Tezos Signed Message:',
+    //   "http://localhost:3000",
+    //   new Date().toISOString,
+    //   packet,
+    //   packet,
+    // ].join(' ');
+
+    // const bytes: string = char2Bytes(formattedInput);
+    // const payloadBytes = '05' + '0100' + char2Bytes(bytes) + bytes;
+
+    // const payload_2: RequestSignPayloadInput = {
+    //   signingType: SigningType.MICHELINE,
+    //   payload: payloadBytes,
+    //   sourceAddress: values.address,
+    // };
+    // const signedPayload = await wallet.client.requestSignPayload(payload_2);
+    // const { signature } = signedPayload;
+
+    await fetch(values.nodeUri + user_operation_gossip,
       {
         method: "POST",
         body: JSON.stringify(packet)
@@ -178,7 +244,7 @@ const mint_cursor = async (
   } catch (err) {
     console.error(err);
   } finally {
-    if (passive_mint_activated && counter_cursor == 0) {
+    if (passive_mint_activated && counter_cursor === 0) {
       setInterval(() => {
         for (let i = 0; i <= (values.cursor_cps * 10); i++) {
           mint(values, actions, action_type.increment_cookie);
@@ -215,9 +281,45 @@ const mint_grandma = async (
   } catch (err) {
     console.error(err);
   } finally {
-    if (passive_mint_activated && counter_grandma == 0) {
+    if (passive_mint_activated && counter_grandma === 0) {
       setInterval(() => {
         for (let i = 0; i <= values.grandma_cps; i++) {
+          mint(values, actions, action_type.increment_cookie);
+          console.log("Grandma: passive mint");
+        }
+      }, 1000);
+      actions.setSubmitting(false);
+    }
+  }
+}
+const mint_farm = async (
+  values: Values,
+  actions: FormikHelpers<Values>,
+  passive_mint_activated: boolean,
+  counter_farm: number,
+) => {
+  actions.setSubmitting(true);
+  try {
+    await mint(values, actions, action_type.increment_farm);
+
+    const state = await get_actual_state(values);
+    values.farms = state.cookie_baker.number_of_farm;
+    values.farm_cost = state.cookie_baker.farm_cost;
+    values.farm_cps = state.cookie_baker.farm_cps;
+    const cookies = state.cookie_baker.number_of_cookie
+    console.log("new total of cookies: " + cookies);
+    values.cookies = cookies;
+    // ADD POST ACTION BELOW
+    setTimeout(() => {
+      actions.setSubmitting(false);
+    }, 500);
+    return state.cookie_baker.farm_cost;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (passive_mint_activated && counter_farm === 0) {
+      setInterval(() => {
+        for (let i = 0; i <= values.farm_cps; i++) {
           mint(values, actions, action_type.increment_cookie);
           console.log("Grandma: passive mint");
         }
@@ -235,6 +337,7 @@ const App = () => {
   //not working, to rework and to add to disabled property for each button
   let isCursorBuyable: boolean;
   let isGrandmaBuyable: boolean;
+  let isFarmBuyable: boolean;
 
   // global boolean to set the tick
   let isPassiveMintActivated: boolean;
@@ -242,6 +345,7 @@ const App = () => {
   // basic counter to avoid acivate several times the passive mint of each category
   let counter_cursor: number = 0;
   let counter_grandma: number = 0;
+  let counter_farm: number = 0;
 
   return (
     <div className="App">
@@ -253,11 +357,14 @@ const App = () => {
           cookies: 0,
           cursors: 0,
           grandmas: 0,
+          farms: 0,
           cursor_cost: 0,
           grandma_cost: 0,
+          farm_cost: 0,
           cps: 0,
           cursor_cps: 0,
-          grandma_cps: 0
+          grandma_cps: 0,
+          farm_cps: 0
         }}
         onSubmit={(values, actions) => {
           console.log("On submit");
@@ -282,6 +389,15 @@ const App = () => {
               isGrandmaBuyable = true;
             } else {
               isGrandmaBuyable = false;
+            }
+          } else if (submitAction === action_type.increment_farm) {
+            isPassiveMintActivated = true;
+            mint_farm(values, actions, isPassiveMintActivated, counter_farm);
+            counter_farm += 1;
+            if (values.cookies >= values.farm_cost) {
+              isFarmBuyable = true;
+            } else {
+              isFarmBuyable = false;
             }
           }
         }}
@@ -335,9 +451,19 @@ const App = () => {
             <label htmlFor="grandma_cost">Next grandma cost:</label>
             <Field id="grandma_cost" name="grandma_cost" />
           </div>
+          <div >
+            <label htmlFor="farms">Farms:</label>
+            <Field id="farms" name="farms" />
+            <button type="submit" disabled={isSubmitting} name="buy_farm"
+              onClick={() => {
+                submitAction = action_type.increment_farm;
+                handleSubmit();
+              }} > <img src={farm} className="Buttons" alt="logo" />
+            </button>
+            <label htmlFor="farm_cost">Next farm cost:</label>
+            <Field id="farm_cost" name="farm_cost" />
+          </div>
 
-          <label htmlFor="Farms">Farms:</label>
-          <Field id="farms" name="farms" />
         </Form>)}
       </Formik>
     </div >)
