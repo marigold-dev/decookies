d36f7e3173dbcad96cc1e1ced5ad9c38f8815b30
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-22 01:39:46 +0200

    settings/projects: add the deku-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,32 @@ import { Game } from './pages/game';
 import { GameProvider } from './store/provider';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { DekuToolkit } from 'deku-toolkit';
+import { useEffect } from 'react';
 
-const App = () =>
-  <GameProvider>
+const deku = new DekuToolkit({ dekuRpc: "http://localhost:8080" });
+
+const App = () => {
+  useEffect(() => {
+    const id = setInterval(() => {
+      deku.level()
+        .then(level => console.log(`The level of the chain is: ${level}`))
+        .catch(console.error);
+    }, 4000);
+    return () => {
+      clearInterval(id);
+    }
+  }, [])
+
+
+  return <GameProvider>
     <Header />
     <div className="App">
       <Game />
     </div>
     <Footer />
   </GameProvider>;
+}
+
 
 export default App;
