bd1a5d2a8248cda01c0b3bdfd67ed54b22324a31
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-10-13 16:58:43 +0200

    use Deku toolkit

--- src/store/vmApi.ts
+++ src/store/vmApi.ts
@@ -8,6 +8,8 @@ import { cookieBakerToLeaderBoard, leaderBoard, vmOperation } from './vmTypes';
 import { keyPair, state } from './reducer'
 import { action, saveUserAddress } from './actions';
 
+import * as deku from '@marigold-dev/deku-toolkit'
+
 export const requestBlockLevel = async (nodeUri: string): Promise<number> => {
     const blockRequest = await fetch(nodeUri + "/api/v1/chain/level",
         {
@@ -56,7 +58,7 @@ const getRawLeaderBoard = async (nodeUri: string): Promise<any> => {
         });
     const stateResponse = JSON.parse(await stateRequest.text(), parseReviver);
     const sorted =
-        Object.entries(stateResponse).sort((a, b) =>  {
+        Object.entries(stateResponse).sort((a, b) => {
             const eatenA = JSON.parse(a[1] as any, parseReviver).eatenCookies;
             const eatenB = JSON.parse(b[1] as any, parseReviver).eatenCookies;
             return Number(eatenB - eatenA);
@@ -82,44 +84,54 @@ export const getLeaderBoard = async (nodeUri: string): Promise<leaderBoard[]> =>
  */
 export const mint = async (action: vmOperation, nodeUri: string, latestState: React.MutableRefObject<state>, dispatch: React.Dispatch<action>): Promise<string> => {
     const keyPair = latestState.current.generatedKeyPair;
-    if (keyPair) {
+    if (keyPair && latestState.current.nodeUri) {
         try {
-            const signer = new InMemorySigner(keyPair.privateKey)
+            const inMemorySigner = new InMemorySigner(keyPair.privateKey)
+            const dekuSigner = deku.fromMemorySigner(inMemorySigner);
+            const dekuToolkit =
+                new deku.DekuToolkit(
+                    {
+                        dekuRpc: latestState.current.nodeUri,
+                        dekuSigner
+                    }
+                );
+            const hash =
+                await dekuToolkit.submitVmOperation(JSON.stringify(action, stringifyReplacer));
 
-            const address = await signer.publicKeyHash();
-            const key = await signer.publicKey();
+            // const address = await signer.publicKeyHash();
+            // const key = await signer.publicKey();
 
-            const level = await requestBlockLevel(nodeUri);
-            const payload = action;
-            const content = ["Vm_transaction", {
-                operation: JSON.stringify(payload, stringifyReplacer),
-                tickets: []
-            }];
-            const source = address;
-            let nonce = createNonce().toString();
-            const operation = {
-                level,
-                nonce,
-                source,
-                content
-            };
-            const signature = await signer.sign(stringToHex(JSON.stringify(operation))).then((val) => val.prefixSig);
-            const fullPayload = JSON.stringify({
-                key,
-                signature,
-                operation
-            }, stringifyReplacer);
+            // const level = await requestBlockLevel(nodeUri);
+            // const payload = action;
+            // const content = ["Vm_transaction", {
+            //     operation: JSON.stringify(payload, stringifyReplacer),
+            //     tickets: []
+            // }];
+            // const source = address;
+            // let nonce = createNonce().toString();
+            // const operation = {
+            //     level,
+            //     nonce,
+            //     source,
+            //     content
+            // };
+            // const signature = await signer.sign(stringToHex(JSON.stringify(operation))).then((val) => val.prefixSig);
+            // const fullPayload = JSON.stringify({
+            //     key,
+            //     signature,
+            //     operation
+            // }, stringifyReplacer);
 
-            const hash = await fetch(nodeUri + "/api/v1/operations",
-                {
-                    method: "POST",
-                    body: fullPayload
-                });
+            // const hash = await fetch(nodeUri + "/api/v1/operations",
+            //     {
+            //         method: "POST",
+            //         body: fullPayload
+            //     });
             //TODO: here?
             // const inOven = latestState.current.cookiesInOven + 1n;
             // console.log("inOven: ", inOven);
             // dispatch(updateOven(inOven));
-            return hash.text();
+            return hash;
         } catch (err) {
             const error_msg = (typeof err === 'string') ? err : (err as Error).message;
             throw new Error(error_msg);;
