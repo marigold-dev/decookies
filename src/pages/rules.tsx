0bf941e49c93062de024adb917396b5cec959bb5
Vanessa Montes <vanessamontesb@gmail.com>
2022-10-28 17:39:30 -0500

    Add some game page components

--- src/pages/rules.tsx
+++ src/pages/rules.tsx
@@ -10,11 +10,11 @@ const Rules = () =>
             <h2>Enjoy making cookies!</h2>
             <p>Rules are easy</p>
             <ul>
-                <li>Click on cookie to bake one </li>
-                <li>When you have enough cookies  buy a building. Each building =&gt; will passively 
+                <li>Click on the giant cookie to bake one cookie! </li>
+                <li>When you have enough cookies  buy a building! Each building =&gt; will passively 
                     mint cookies for you. The more buildings of the same type you buy, the more the price inscreases</li>
-                <li>You can give cookies to a friend, because it is always great to get cookies from a friends</li>
-                <li>Eat cookies allow you to “burn” cookies and appear in the leaderboard. Who will the best cookie eater?</li>
+                <li>You can give cookies to a friend, because it is always great to get cookies from friends</li>
+                <li>"Eat cookies" allow you to “burn” cookies and appear in the leaderboard. Who will the best cookie eater?</li>
             </ul>
         </section>
         <section className="right">
