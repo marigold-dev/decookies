436b710dcd2367ae2bf046e5c5686fd9271cf4db
Vanessa Montes <vanessamontesb@gmail.com>
2022-11-01 08:17:01 -0500

    Add mobile  style complete

--- src/components/game/container.ts
+++ src/components/game/container.ts
@@ -1,78 +1,98 @@
 import styled from "styled-components";
-import left from '../../../resources/images/left-bg.png';
+import left from "../../../resources/images/left-bg.png";
 
 const GameContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    font-family: 'Roboto Mono', monospace;
-    .right {
-        display:none;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  font-family: "Roboto Mono", monospace;
+  .right {
+    display: none;
+  }
+  .left {
+    display: block;
+    width: 207px;
+    margin: -1.3em 1px 25em 0em;
+    background-image: url(${left});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    .container {
+      padding: 1em 1em;
+    }
+  }
+  .middle {
+    color: white;
+    text-align: center;
+    button {
+      background: #1c1d22;
+      border: none;
+    }
+    p {
+      text-align: left;
+      font-size: 12px;
+      padding-left: 2.8em;
+    }
+    .cookieText {
+      color: #858080;
+      text-align: left;
+      margin-top: -10px;
+      padding: 0 0 3em 0;
+      font-size: 9px;
+      padding-left: 3.4em;
     }
     .cookieBtn {
-        height: 50vmin;
-        pointer-events: none;
-        background:#1C1D22;
-                }
-    @media (min-width: 900px) {
-            &.container {
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-gap: 10px;
-        }
-        section {
-            &.middle {
-                color: white;
-                text-align: center;
-                button {
-                    background: #1C1D22;
-                    border: none;
-                }
-                p {
-                    text-align:left;
-                    font-size: 18px;
-                    padding-left: 2.8em;
-                }
-                .cookieText {
-                    color:#858080;
-                    text-align:left;
-                    margin-top: -10px;
-                    padding: 0 0 3em 0;
-                    font-size: 14px;
-                    padding-left: 3.4em;
-
-                }
-                .cookieBtn {
-                    height: 37vmin;
-                    pointer-events: none;
-                    animation: animate 7s linear infinite;
-                    background:#1C1D22;
-                }
-            }
-            &.right {
-                display:flex;
-                flex-direction:column;
-            }
-            &.left {
-                width: 350px;
-                margin: -1.3em 0px -5em 4em;
-                background-image: url(${left});
-                background-size: cover;
-                background-position: center;
-                background-repeat: no-repeat;
-                overflow-x: hidden;
-                overflow-y: scroll;
-               .container {
-                padding: 1em 1em;
-               }
-            }
-        }
+      height: 25vmin;
+      pointer-events: none;
+      animation: animate 7s linear infinite;
+      background: #1c1d22;
     }
-    @keyframes animate {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-        transform: rotate(360deg);
-        }
+  }
+  @media (min-width: 900px) {
+    &.container {
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-gap: 10px;
     }
+    section {
+      &.middle {
+        .cookieBtn {
+          height: 37vmin;
+        }
+        p {
+          font-size: 18px;
+        }
+        .cookieText {
+          font-size: 14px;
+        }
+      }
+      &.right {
+        display: flex;
+        flex-direction: column;
+      }
+      &.left {
+        display: block;
+        width: 350px;
+        margin: -1.3em 0px -5em 4em;
+        background-image: url(${left});
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        .container {
+          padding: 1em 1em;
+        }
+      }
+    }
+  }
+  @keyframes animate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
-export default GameContainer
+export default GameContainer;
--- src/components/game/gameButton.ts
+++ src/components/game/gameButton.ts
@@ -1,64 +1,96 @@
 import styled from "styled-components";
 
 const GameButton = styled.button`
-    background: none;
-    border: 0;
-    color: inherit;
-    font: inherit;
-    line-height: normal;
-    overflow: visible;
-    padding: 0;
-    div {
-        &.gameButtonContainer {
-            display:grid;
-            grid-template-columns: 1fr 1fr 1fr 1fr;
-            justify-content:center;
-            align-items:center;
-            padding: 0 1em 0.5em 1em;
-            height: 6.5em;
-            overflow-y: scroll;
-            font-size:12px;
-            color: #858080;
-            h3 {
-                color: white;
-            }
-            .background {
-                justify-content:center;
-                display:flex;
-                align-items:center;
-                background:#2B2A2E;
-                margin: 0.3em;
-                height:74px;
-            }
-            .title {
-                margin: 0.5em 0 0.7em 0;
-                text-align: left;
-            }
-            img {
-                width:30px;
-                height:30px;
-                padding: 0 1em;
-                &.price {
-                    width:12px;
-                    height:12px;
-                    padding-right:1em;
-                    margin-left:-1em;
-                }
-            }
-        }
-        &.column {
-            display:flex;
-            flex-direction:column;
-            width:90px;
-            &.price {
-                display:flex;
-                flex-direction:row;
-                }
-            }
-        }
-@media (min-width: 900px) {
-
+  background: none;
+  border: 0;
+  color: inherit;
+  font: inherit;
+  line-height: normal;
+  overflow: visible;
+  padding: 0;
+  .gameButtonContainer {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+    padding: 0 0.5em 0.2em 0.5em;
+    height: auto;
+    overflow-y: scroll;
+    font-size: 7px;
+    color: #858080;
+    width: 195px;
+    h3 {
+      color: white;
     }
+    .background {
+      justify-content: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background: #2b2a2e;
+      margin: 0.3em;
+      height: 40px;
+    }
+    .title {
+      margin: 0.5em 0 0.7em 0;
+      text-align: left;
+    }
+    img {
+      width: 15px;
+      height: 15px;
+      padding: 0 0.5em;
+      &.price {
+        width: 12px;
+        height: 12px;
+        padding-right: 1em;
+        margin-left: -1em;
+      }
+    }
+  }
+  &.column {
+    display: flex;
+    flex-direction: column;
+    width: 60px;
+    &.price {
+      display: flex;
+      flex-direction: row;
+    }
+  }
+  @media (min-width: 900px) {
+    div {
+      &.gameButtonContainer {
+        padding: 0 1em 0.5em 1em;
+        height: 6.5em;
+        font-size: 12px;
+        width: auto;
+        h3 {
+          color: white;
+        }
+        .background {
+          margin: 0.3em;
+          height: 74px;
+        }
+        img {
+          width: 30px;
+          height: 30px;
+          padding: 0 1em;
+          &.price {
+            width: 12px;
+            height: 12px;
+            padding-right: 1em;
+            margin-left: -1em;
+          }
+        }
+      }
+      &.column {
+        width: 90px;
+        &.price {
+          display: flex;
+          flex-direction: row;
+        }
+      }
+    }
+  }
 `;
 
-export default GameButton
+export default GameButton;
